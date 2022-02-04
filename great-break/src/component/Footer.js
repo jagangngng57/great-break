68c9b68f3ef21a2dded7dc968730a040d6747888
Jagadeesh <jagadeesh.ks@qapitol.com>
2022-02-04 07:40:19 +0530

    structure of project

---
+++ great-break/src/component/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div>
+            <hr/>
+            <center>&copy; great-break Inc</center>
+        </div>
+    )
+}
+
+export default Footer
---
+++ great-break/src/component/Home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from './Header';
+import Footer from './Footer';
+
+const Home = () => {
+    return (
+        <div>
+            <Header/>
+            <center>Coming to Kosigi</center>
+            <center>In Progress</center>
+            <Footer/>
+        </div>
+    )
+}
+
+export default Home
